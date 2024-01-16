ee73b1475949375b822e6b4ddb0c795e16f65034
luoyi <luoyi58624@163.com>
2024-01-16 16:06:24 +0800

    移除dev环境

--- vite.config.ts
+++ vite.config.ts
@@ -6,48 +6,44 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VarletUIResolver, ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig(({ command, mode }) => {
-	const env = loadEnv(mode, process.cwd(), '')
-	return {
-		base: '/mowork_app_web/',
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, 'src')
-			}
-		},
-		plugins: [
-			vue(),
-			UnoCSS(),
-			AutoImport({
-				resolvers: [VarletUIResolver({ autoImport: true }), ElementPlusResolver()],
-				imports: ['vue', 'pinia'],
-				dirs: ['./src/store', './src/http', './src/utils', './src/config', './src/composables'],
-				dts: true
-			}),
-			Components({
-				resolvers: [VarletUIResolver(), ElementPlusResolver()],
-				extensions: ['vue', 'tsx'],
-				deep: true,
-				directoryAsNamespace: false,
-				dirs: ['src/components'],
-				dts: true
-			})
-		],
-		build: {
-			outDir: env.VITE_BUILD_DIR,
-			rollupOptions: {
-				output: {
-					entryFileNames: `js/app.${new Date().getTime()}.js`,
-					chunkFileNames: `js/[name].${new Date().getTime()}.js`,
-					assetFileNames: `assets/[name].${new Date().getTime()}.[ext]`,
-					compact: true,
-					manualChunks: {
-						'vendors/vue': ['vue', 'vue-router', 'pinia'],
-						'vendors/common': ['lodash-es', 'dayjs', 'axios', 'nprogress'],
-						'vendors/element-ui': ['element-plus'],
-						'vendors/element-admin-layout': ['element-admin-layout'],
-						'vendors/varlet-ui': ['@varlet/ui', '@varlet/touch-emulator']
-					}
+export default defineConfig({
+	base: '/mowork_app_web/',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	plugins: [
+		vue(),
+		UnoCSS(),
+		AutoImport({
+			resolvers: [VarletUIResolver({ autoImport: true }), ElementPlusResolver()],
+			imports: ['vue', 'pinia'],
+			dirs: ['./src/store', './src/http', './src/utils', './src/config', './src/composables'],
+			dts: true
+		}),
+		Components({
+			resolvers: [VarletUIResolver(), ElementPlusResolver()],
+			extensions: ['vue', 'tsx'],
+			deep: true,
+			directoryAsNamespace: false,
+			dirs: ['src/components'],
+			dts: true
+		})
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `js/app.${new Date().getTime()}.js`,
+				chunkFileNames: `js/[name].${new Date().getTime()}.js`,
+				assetFileNames: `assets/[name].${new Date().getTime()}.[ext]`,
+				compact: true,
+				manualChunks: {
+					'vendors/vue': ['vue', 'vue-router', 'pinia'],
+					'vendors/common': ['lodash-es', 'dayjs', 'axios', 'nprogress'],
+					'vendors/element-ui': ['element-plus'],
+					'vendors/element-admin-layout': ['element-admin-layout'],
+					'vendors/varlet-ui': ['@varlet/ui', '@varlet/touch-emulator']
 				}
 			}
 		}
